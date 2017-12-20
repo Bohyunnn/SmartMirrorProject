0dc6bb5e9957b8bea2445150dc37d9a0303f9729
Bohyunnn <kbh236@naver.com>
2017-12-20 12:05:13 +0900

    smart-mirror 음성인식 Application

---
+++ app/src/main/java/com/example/hansung/smartmirrorproject/SetIPFragment.java
@@ -0,0 +1,58 @@
+package com.example.hansung.smartmirrorproject;
+
+import android.content.SharedPreferences;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SetIPFragment extends Fragment {
+
+
+    private EditText et_ip;
+    private TextView tv_currentIP;
+    private Button btn_setIP;
+
+    private String current_ip; // 환경변수에 저장된 ip주소
+    private String smartmirror_ip; // 스마트 미러의 할당받은 ip 주소
+
+    /* 환경 변수 */
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_set_ipfragment, container, false);
+
+        tv_currentIP = (TextView)rootView.findViewById(R.id.tv_currentIP);
+        et_ip = (EditText)rootView.findViewById(R.id.et_ip);
+        btn_setIP = (Button) rootView.findViewById(R.id.btn_setIP);
+
+        prefs = getActivity().getSharedPreferences("login", 0);
+        editor = prefs.edit();
+//192.168.0.29
+        current_ip = prefs.getString("SMARTMIRROR_IP","192.168.0.32");
+        tv_currentIP.setText(current_ip);
+
+        btn_setIP.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                smartmirror_ip = et_ip.getText().toString();
+                editor.putString("SMARTMIRROR_IP",smartmirror_ip);
+                editor.commit();
+                Toast.makeText(getActivity(),"IP가"+ smartmirror_ip +"로 설정되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return rootView;
+    }
+}
